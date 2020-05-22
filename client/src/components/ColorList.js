d7493233c3ba03773cee8eb3ded07ec60d088c88
MatthewSchuette <mjschuette4@gmail.com>
2020-05-22 11:08:12 -0500

    Fixed a login bug where it kept saying credentials were wrong

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from '../Utilities/axiosWithAuth';
 
 const initialColor = {
   color: "",
@@ -21,6 +21,13 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        console.log(res.data);
+        updateColors();
+      })
+      .catch(err => console.log("colorList put error -", err));
   };
 
   const deleteColor = color => {
